1256f5a40cabe30506301cb4601ec8fcedd998ee
Danila <serpokrylov.danila@mail.ru>
2021-04-25 16:58:50 +0300

    added new folder react-2 - my first react app

---
+++ react-2/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () =>{
+    return <header className='header'>
+        <img src='https://api.freelogodesign.org/assets/thumb/logo/21833077_400.png' />
+    </header>
+}
+
+export default Header;
